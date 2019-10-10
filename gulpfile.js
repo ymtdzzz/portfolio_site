16b7633adc507b96430f8482696b9efd313bf749
zeroclock <zeroclock@icloud.com>
2019-10-10 23:05:49 +0900

    gulp導入

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var fs = require('fs');
+var pkg = JSON.parse(fs.readFileSync('./package.json'));
+var assetsPath = path.resolve(pkg.path.assetsDir);
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+
+gulp.task('sass', function() {
+    return gulp.src(path.join(assetsPath, 'sass/style.scss'))
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.join(assetsPath, 'css/')));
+});
+
+gulp.task('default', function() {
+    gulp.watch(path.join(assetsPath, 'sass/**/*.scss'), gulp.parallel('sass'));
+});
